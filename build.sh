7695c71a32706ff09991e2a2888eebb2749be76e
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2021-01-23 23:41:54 -0500

    Add termux ld and c flags

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-export CGO_LDFLAGS="-L/usr/lib -L/usr/local/lib -lssh -lgcrypt -lgpg-error -lz"
-export CGO_CFLAGS="-I/usr/include -I/usr/local/include"
+if [ $# -gt 0 ] && [ "$1" == "termux" ]; then
+	export CGO_LDFLAGS="-L/system/lib64 -L/data/data/com.termux/files/lib -L/data/data/com.termux/files/usr/lib -lssh -lgcrypt -lgpg-error -lz"
+	export CGO_CFLAGS="-I/usr/include -I/usr/local/include -I/data/data/com.termux/files/usr/include -I/data/data/com.termux/files/usr/local/include"
+else
+	export CGO_LDFLAGS="-L/usr/lib -L/usr/local/lib -lssh -lgcrypt -lgpg-error -lz"
+	export CGO_CFLAGS="-I/usr/include -I/usr/local/include"
+fi
+
+echo $CGO_LDFLAGS
+echo $CGO_CFLAGS
 
 go build .
 
